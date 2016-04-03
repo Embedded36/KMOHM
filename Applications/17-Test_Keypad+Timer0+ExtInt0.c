9934277d1ccd1666b895f531e682fba449440bb3
Kareem Galal <Eng.KareemGalal@gmail.com>
2016-04-03 19:41:43 +0200

    stable version

--- Applications/17-Test_Keypad+Timer0+ExtInt0.c
+++ Applications/17-Test_Keypad+Timer0+ExtInt0.c
@@ -5,80 +5,101 @@
  Author: KiMo
  */
 
-#include "Utilities\Types.h"
-#include "Utilities\Delay.h"
-#include "MCAL\DIO\DIO_Interface.h"
-#include "HAL\KEYPAD\KEYPAD_Interface.h"
+#include "..\Utilities\Types.h"
+#include "..\Utilities\Delay.h"
+#include "..\MCAL\DIO\DIO_Interface.h"
+#include "..\MCAL\TIMER\TIMER_Interface.h"
+#include "..\MCAL\EXTINT\EXTINT_Interface.h"
+//#include "..\HAL\TSW\TSW_Interface.h"
+#include "..\HAL\KEYPAD\KEYPAD_Interface.h"
 
+#define OneHalfSec	(976)
 
+#define REDPIN		8
+#define BLUEPIN		9
+#define GREENPIN	10
+
+#define REDLED			1
+#define BLUELED			2
+#define GREENLED		3
+
+volatile u8 OnOffFlag = 0;
+
+void ToggleLedStatus(void) {
+	//__asm__("CLI");
+	OnOffFlag = !OnOffFlag;
+	//__asm__("SEI");
+}
 
 int main(void) {
 	//u8 Local_u8SwitchValue; //To store the switch value
 	u8 Local_u8KeypadValue = 0; //Counter to adjust the delay
-	u8 Previous_Value = 0;
+	u32 Local_u32Timer0OverFlowNum;
+	u8 Local_u8LEDONOFF = 0;
+	u8 Local_u8WhichLed = REDLED;
 	DIO_voidInit();
+	EXTINT_voidInit();
+	EXTINT_u8AttachInt(EXTINT0, ToggleLedStatus);
+	__asm__("SEI");
+	TIMER0_voidInit();
 	KEYPAD_voidInit();
+
 	while (1) {
+		switch (Local_u8WhichLed) {
+		case REDLED:
+			if (!OnOffFlag) {
+				DIO_u8WritePinVal(REDPIN, DIO_u8LOW);
+			} else {
+				Local_u32Timer0OverFlowNum = TIMER0_u32GetOverflowCount();
+				if (Local_u32Timer0OverFlowNum >= OneHalfSec) {
+					TIMER0_voidClearOverflowCount();
+					Local_u8LEDONOFF = !Local_u8LEDONOFF;
+					DIO_u8WritePinVal(REDPIN, Local_u8LEDONOFF);
+				}
+			}
+			break;
+		case BLUELED:
+			if (!OnOffFlag) {
+				DIO_u8WritePinVal(BLUEPIN, DIO_u8LOW);
+			} else {
+				Local_u32Timer0OverFlowNum = TIMER0_u32GetOverflowCount();
+				if (Local_u32Timer0OverFlowNum >= OneHalfSec) {
+					TIMER0_voidClearOverflowCount();
+					Local_u8LEDONOFF = !Local_u8LEDONOFF;
+					DIO_u8WritePinVal(BLUEPIN, Local_u8LEDONOFF);
+				}
+			}
+			break;
+		case GREENLED:
+			if (!OnOffFlag) {
+				DIO_u8WritePinVal(GREENPIN, DIO_u8LOW);
+			} else {
+				Local_u32Timer0OverFlowNum = TIMER0_u32GetOverflowCount();
+				if (Local_u32Timer0OverFlowNum >= OneHalfSec) {
+					TIMER0_voidClearOverflowCount();
+					Local_u8LEDONOFF = !Local_u8LEDONOFF;
+					DIO_u8WritePinVal(GREENPIN, Local_u8LEDONOFF);
+				}
+			}
+			break;
+		default: break;
+		}
+
 		KEYPAD_u8Read(&Local_u8KeypadValue);
 		switch (Local_u8KeypadValue) {
-		case 0:
-			if(Previous_Value == 0) StepperOFF();
-			else StepperRunning(Previous_Value);
-			break; //0
-//		case 1:
-//			StepperRunning(Local_u8KeypadValue);
-//			break; //1
-//		case 2:
-//			StepperRunning(Local_u8KeypadValue);
-//			break; //2
-//		case 3:
-//			StepperRunning(Local_u8KeypadValue);
-//			break; //3
-//		case 4:
-//			StepperRunning(Local_u8KeypadValue);
-//			break; //4
-//		case 5:
-//			StepperRunning(Local_u8KeypadValue);
-//			break; //5
-//		case 6:
-//			StepperRunning(Local_u8KeypadValue);
-//			break; //6
-//		case 7:
-//			StepperRunning(Local_u8KeypadValue);
-//			break; //7
-//		case 8:
-//			StepperRunning(Local_u8KeypadValue);
-//			break; //8
-//		case 9:
-//			StepperRunning(Local_u8KeypadValue);
-//			break; //9
-//		case 10:
-//			StepperRunning(Local_u8KeypadValue);
-//			break; //10
-//		case 11:
-//			StepperRunning(Local_u8KeypadValue);
-//			break; //11
-//		case 12:
-//			StepperRunning(Local_u8KeypadValue);
-//			break; //12
-//		case 13:
-//			StepperRunning(Local_u8KeypadValue);
-//			break; //13
-//		case 14:
-//			StepperRunning(Local_u8KeypadValue);
-//			break; //14
-//		case 15:
-//			StepperRunning(Local_u8KeypadValue);
-//			break; //15
-		case 16:
-			StepperOFF();
-			Previous_Value = 0;
-			break; //16
+		case 1:
+			Local_u8WhichLed = REDLED;
+			break;
+		case 2:
+			Local_u8WhichLed = BLUELED;
+			break;
+		case 3:
+			Local_u8WhichLed = GREENLED;
+			break;
 		default:
-			Previous_Value = Local_u8KeypadValue;
-			StepperRunning(Local_u8KeypadValue);
 			break;
 		}
 	}
+	//TSW_u8CheckSwitch(TSW_u8NUM1, &Local_u8SwitchValue);
 	return 0;
 }
