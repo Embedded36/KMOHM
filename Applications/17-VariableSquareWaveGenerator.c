abb09f7ca769dedc718d8e49ce9424798f2a57ae
Kareem Galal <Eng.KareemGalal@gmail.com>
2016-05-02 01:04:41 +0200

    Completed Variable Square Wave Genarator, Duty Cycle 50%

--- Applications/17-VariableSquareWaveGenerator.c
+++ Applications/17-VariableSquareWaveGenerator.c
@@ -12,29 +12,23 @@
  5-Assign this value to OCR: O/P compare Register
  */
 
-
-
 #include "..\Utilities\Types.h"
 #include "..\Utilities\Interrupt.h"
 #include "..\MCAL\DIO\DIO_Interface.h"
 #include "..\MCAL\ADC\ADC_Interface.h"
 #include "..\MCAL\TIMER\TIMER_Interface.h"
-//#include "..\HAL\PotentioMeter\PotentioMeter_Interface.h"
 
 int main(void) {
-	//f32 Local_f32PotentioVal;
 	u8 Local_u8OutputCompareValue;
 	u16 Local_u16ADCValue;
 	u16 Local_u16ADCValueP;
 	DIO_voidInit();
 	ADC_voidInit();
-	TIMER0_voidInit();
 	TIMER0_voidSetOCR(255);
-	//TIMER_u8AttachInt(TIMER0_COMP, VariableSquareWave);
+	TIMER0_voidInit(); //TIMER0_CTC, TIMER0_COM1
 	//Enable_Global_INT();
 	// enable global interrupt
 	while (1) {
-		//PotentioMeter_u8GetVoltageVal(POTENTIOMETER_u8NUM1, &Local_f32PotentioVal);
 		ADC_u8ReadChannel(ADC_u8CH0, &Local_u16ADCValue);
 		if (Local_u16ADCValue != Local_u16ADCValueP) {
 			Local_u8OutputCompareValue = (u8)(Local_u16ADCValue >> 2);
